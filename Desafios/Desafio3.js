dfdfc20d6054d95224e16809b86df3e928781984
Henri Santos <59731545+HenriSantosCode@users.noreply.github.com>
2025-10-24 22:41:52 -0300

    Update Desafio3.js

--- Desafios/Desafio3.js
+++ Desafios/Desafio3.js
@@ -1,1 +1,35 @@
+class Heroi{
+    constructor(nome, idade, tipo, ataque){
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+
+         switch(this.tipo) {
+            case "mago":
+                this.ataque = "magia";
+                break;
+            case "guerreiro":
+                this.ataque = "espada";
+                break;
+            case "monge":
+                this.ataque = "artes marciais";
+                break;
+            case "ninja":
+                this.ataque = "shuriken";
+                break;
+}
+    } 
+    atacar(){
+        console.log(`O ${this.tipo} atacou usando ${this.ataque}`);
+    }
+    
+    
+}
+
+let heroi1 = new Heroi("Henrique", 22, "guerreiro");
+
+heroi1.atacar();
+
+
+
 
